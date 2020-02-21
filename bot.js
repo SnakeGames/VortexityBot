86dccb72fbd339a7175d272094f57d84eb59ce5b
SnakeGames <61299588+SnakeGames@users.noreply.github.com>
2020-02-20 23:45:41 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js")
-const Client  = new Discord.Client();
+const bot = new Discord.client();
 const prefix = "-"
 
 const token ='NjgwMzA2NTk2ODUyNzkzMzU1.Xk9_Dw.0_jVLJN6G2Si1Cd9UIie3MSeBmo';
@@ -7,11 +7,11 @@ const token ='NjgwMzA2NTk2ODUyNzkzMzU1.Xk9_Dw.0_jVLJN6G2Si1Cd9UIie3MSeBmo';
 
 
 
-Client.on('ready', ()=>{
+bot.on('ready', ()=>{
     console.log("The bot has been logged in!")
 })
 
-Client.on('message', (message)=>{
+bot.on('message', (message)=>{
     if(-message.content.startsWith.apply(prefix)) return;
     if(message.author.bot) return;
 
@@ -28,4 +28,4 @@ Client.on('message', (message)=>{
         message.author.send("Hello! Applications are currently closed! Sorry!")
 })
 
-Client.login('proccess.env.token');
+bot.login(process.env.token);
