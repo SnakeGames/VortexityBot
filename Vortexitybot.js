5b597237a5d82ae48ea74f8e04b9b8a5db32515f
SnakeGames <61299588+SnakeGames@users.noreply.github.com>
2020-02-25 14:25:25 -0800

    Update Vortexitybot.js

--- Vortexitybot.js
+++ Vortexitybot.js
@@ -10,20 +10,22 @@ Client.on('message', (message)=>{
     if(-message.content.startsWith.apply(prefix)) return;
     if(message.author.bot) return;
 
+       let args = meessage.content.substring(prefix.legnth).split(" ")
+
+       switch(args[0]){
+           case 'help':
+                const help = new Discord.RichEmbed()
+                .addField('Player Information', msg.author.username)
+                message.channel.sendEmbed(embed);
+          break;
+       }
+
 
     if(message.content.startsWith(prefix + "ip"))
         message.reply("Currently, there is no ip! We are working on the server!");
 
-
-    if(message.content.startsWith(prefix + "help"))
-        message.author.send("The commands are ```-ip -apply```");
-
-
     if(message.content.startsWith(prefix + "apply"))
         message.author.send("Hello! Applications are currently closed! Sorry!")
-    
-    if(message.content.startsWith(prefix + "website"))
-        message.reply("Hello! The website is currently in development, when its done there will be an announcement!")
 })
 
 Client.login(process.env.token);
